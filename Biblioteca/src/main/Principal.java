29cf6143a42e43652e21f64fe2f159143e42b5c6
ra21603818 <ra21603818@AN01201621077090.dinfor.uniceub>
2017-10-20 20:25:51 -0200

    Commit

---
+++ Biblioteca/src/main/Principal.java
@@ -0,0 +1,14 @@
+package main;
+
+import control.ControleExibeEmprestimos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+			//Criar objetos da ControleExibeEmprestimo
+			ControleExibeEmprestimos cee = new ControleExibeEmprestimos();
+			
+		
+	}
+
+}
